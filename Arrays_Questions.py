902cfb4c4adb4dc5ed108a66edf65b308a7ac9c2
Inceptioner <30520916+Inceptioner@users.noreply.github.com>
2018-11-06 22:27:08 -0500

    Add "Buy and sell a stock once" question solution

--- Arrays_Questions.py
+++ Arrays_Questions.py
@@ -15,3 +15,14 @@ def even_odd(A):
 # increase time complexity, since the array itself has index searching.
 
 
+# buy stock question - given list, buy once and return max profit method.
+def buy_and_sell_stock_once(prices):
+    min_price_so_far, max_profit = float('inf'), 0.0
+    for price in prices:
+        max_profit_sell_today = price - min_price_so_far
+        max_profit = max(max_profit, max_profit_sell_today)
+        min_price_so_far = min(min_price_so_far, price)
+    return max_profit
+
+
+
